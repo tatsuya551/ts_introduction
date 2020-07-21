f510ef4c0cea91f96fe96466227fe5e63b53681d
Seki Tatsuya <seki0515ya@gmail.com>
2020-07-21 13:06:36 +0900

    implement Property Method Accessor decorator

--- dist/decorator.js
+++ dist/decorator.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logging(message) {
     return function (constructor) {
         console.log(message);
@@ -16,7 +19,6 @@ function Component(template, selector) {
         return class extends constructor {
             constructor(...args) {
                 super(...args);
-                console.log("pppp");
                 const mountElement = document.querySelector(selector);
                 const instance = new constructor();
                 if (mountElement) {
@@ -27,13 +29,68 @@ function Component(template, selector) {
         };
     };
 }
+// targetの値は、staticメソッドならclass、static以外のメソッドならプロトタイプ。
+function PropertyLogging(target, propertyKey) {
+    console.log("PropertyLogging-start");
+    console.log(target);
+    console.log(propertyKey);
+    console.log("PropertyLogging-end");
+}
+function MethodLogging(target, propertyKey, descriptor) {
+    console.log("MethodLogging-start");
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+    console.log("MethodLogging-end");
+}
+function enumerable(isEnumerable) {
+    return function (_target, _propertyKey, _descriptor) {
+        return {
+            enumerable: isEnumerable
+        };
+    };
+}
+function AccessorLogging(target, propertyKey, descriptor) {
+    console.log("AccessorLogging-start");
+    console.log(target);
+    console.log(propertyKey);
+    console.log(descriptor);
+    console.log("AccessorLogging-end");
+}
+function ParameterLogging(target, propertyKey, parameterIndex) {
+    console.log("ParameterLogging-start");
+    console.log(target);
+    console.log(propertyKey);
+    console.log(parameterIndex);
+    console.log("ParameterLogging-end");
+}
 let User = class User {
-    constructor(age) {
-        this.age = age;
+    constructor(_age) {
+        this._age = _age;
         this.name = "Quill";
         console.log("User was created!");
     }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
+    }
+    greeting(message) {
+        console.log(message);
+    }
 };
+__decorate([
+    PropertyLogging
+], User.prototype, "name", void 0);
+__decorate([
+    AccessorLogging
+], User.prototype, "age", null);
+__decorate([
+    enumerable(false),
+    MethodLogging,
+    __param(0, ParameterLogging)
+], User.prototype, "greeting", null);
 User = __decorate([
     Component("<h1>{{name}}</h1>", "#app"),
     Logging("LOGGING......")
--- src/decorator.ts
+++ src/decorator.ts
@@ -8,9 +8,8 @@ function Logging(message: string) {
 function Component(template: string, selector: string) {
   return function <T extends { new(...args: any[]): { name: string } }>(constructor: T) {
     return class extends constructor {
-      constructor(...args: any[]){
+      constructor(...args: any[]) {
         super(...args);
-        console.log("pppp")
         const mountElement = document.querySelector(selector);
         const instance = new constructor();
         if (mountElement) {
@@ -22,12 +21,66 @@ function Component(template: string, selector: string) {
   }
 }
 
+// targetの値は、staticメソッドならclass、static以外のメソッドならプロトタイプ。
+function PropertyLogging(target: any, propertyKey: string) {
+  console.log("PropertyLogging-start");
+  console.log(target);
+  console.log(propertyKey);
+  console.log("PropertyLogging-end");
+}
+
+function MethodLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  console.log("MethodLogging-start");
+  console.log(target);
+  console.log(propertyKey);
+  console.log(descriptor);
+  console.log("MethodLogging-end");
+}
+
+function enumerable(isEnumerable: boolean){
+  return function(_target: any, _propertyKey: string, _descriptor: PropertyDescriptor){
+    return {
+      enumerable: isEnumerable
+    }
+  }
+}
+
+function AccessorLogging(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  console.log("AccessorLogging-start");
+  console.log(target);
+  console.log(propertyKey);
+  console.log(descriptor);
+  console.log("AccessorLogging-end");
+}
+
+function ParameterLogging(target: any, propertyKey: string, parameterIndex: number) {
+  console.log("ParameterLogging-start");
+  console.log(target);
+  console.log(propertyKey);
+  console.log(parameterIndex);
+  console.log("ParameterLogging-end");
+}
+
+
 @Component("<h1>{{name}}</h1>", "#app")
 @Logging("LOGGING......")
 class User {
+  @PropertyLogging
   name = "Quill";
-  constructor(public age: number) {
+  constructor(private _age: number) {
     console.log("User was created!")
+  }
+  @AccessorLogging
+  get age () {
+    return this._age;
+  }
+  set age (value) {
+    this._age = value;
+  }
+  @enumerable(false)
+  @MethodLogging
+  greeting(@ParameterLogging message: string) {
+    console.log(message);
   }
 }
 
